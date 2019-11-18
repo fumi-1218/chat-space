e9a94e46e97c0c519f50758c758cd71a4b7db1bd
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-11-18 16:25:49 +0900

    create ajax
    非同期通信の作成を行った

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,1 +1,61 @@
-]
+$(function(){
+  function builderHTML(message){
+    if ( message.image ) {
+      var html = `<div class="maincontents__toptext" data-message-id=${message.id}>
+                    <div class="maincontents__toptext__left">
+                      ${message.user_name}
+                    </div>
+                    <div class="maincontents__toptext__right">
+                      ${message.date}
+                    </div>
+                    <div class="maincontents__bottomtext">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                    <div class="maincontents__bottomtext">
+                      <img src=${message.image} >
+                    </div>
+                  </div>`
+    return html;
+    } else {
+      var html =`<div class="maincontents__toptext" data-message-id=${message.id}>
+                  <div class="maincontents__toptext__left">
+                    ${message.user_name}
+                  </div>
+                  <div class="maincontents__toptext__right">
+                    ${message.date}
+                  </div>
+                  <div class="maincontents__bottomtext">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                </div>`
+    }
+  }
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');     
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
+  });
+});
